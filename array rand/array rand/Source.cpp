fe5c32a8c49d211e41d1776405eef48723737b1a
Александр <@DESKTOP-8UR5T5D>
2023-05-30 00:58:14 +0700

    TWO_MACC

--- array rand/array rand/Source.cpp
+++ array rand/array rand/Source.cpp
@@ -12,23 +12,30 @@ void Print(const int arr[], const int n);
 void PrintTwo(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void UniqueRand(int arr[], int n);
+void UniqueRandTwo(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 
 void shift(int arr[], int n);
 int shiftLeft(int arr[], const int n);
 int shiftRight(int arr[], const int n);
 
 void Search(int arr[], int n);
+void SearchTwo(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 int Sum(const int arr[], const int n);
 double Avg(const int arr[], const int n);
+
 int minValueIn(const int arr[], const int n);
 int maxValueIn(const int arr[], const int n);
+
+//sort
 
 
 
 //int Print_a(const int arr[], const int ROWC, const int  COLS);
 
-#define ONE_MACC
-//#define TWO_MACC
+//#define ONE_MACC
+#define TWO_MACC
 
 
 
@@ -48,8 +55,8 @@ void main()
 		cout << "Сумма всех элементов в массиве: " << Sum(arr, n) << endl;
 		cout << "Среднее арифметическое элементов в массиве: " << Avg(arr, n) << endl;
 		Search(arr, n);
+		cout << "Массив с уникальными элементами: " << endl;
 		UniqueRand(arr, n);
-		cout << "Массив с уникальными элементами: ";
 		Print(arr, n);
 		shift(arr, n);
 		Print(arr, n);
@@ -59,10 +66,15 @@ void main()
 
 #ifdef TWO_MACC
 	{
-
+		setlocale(LC_ALL, "");
 		int arr[ROWS][COLS];
 		FillrandTwo(arr, ROWS, COLS);
 		PrintTwo(arr, ROWS, COLS);
+		SearchTwo(arr, ROWS, COLS);
+		cout << "Массив с уникальными элементами: " << endl;
+		UniqueRandTwo(arr, ROWS, COLS);
+		PrintTwo(arr, ROWS, COLS);
+	
 
 	}
 #endif
@@ -81,7 +93,7 @@ void FillrandTwo(int arr[ROWS][COLS], const int ROWS, const int COLS)
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
-			arr[i][j] = rand() % 100;
+			arr[i][j] = rand() % 12;
 	}
 }
 
@@ -104,74 +116,12 @@ void PrintTwo(int arr[ROWS][COLS], const int ROWS, const int COLS)
 	cout << endl;
 }
 
-
-int Sum(const int arr[], const int n)
-{
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-}
-
-double Avg(const int arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-
-int minValueIn(const int arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-
-int maxValueIn(const int arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-
-void Search(int arr[], int n)
-{
-	int replay = 0;
-	int not_replay = 0;
-	for (int i = 0; i != n; i++)
-	{
-		replay = 0;
-		for (int j = i + 1; j != n; j++)
-		{
-			if (arr[i] == arr[j])
-				replay++;
-		}
-		for (int l = i - 1; l >= 0; l--)
-		{
-			if (arr[i] == arr[l])
-				replay = 0;
-		}
-		if (replay > 0)
-		{
-			cout << "Элемент " << arr[i] << " повторяется " << replay << " раз(а). " << endl;
-			not_replay++;
-		}
-	}
-	if (replay == 0 && not_replay == 0) cout << "Повторяющихся элементов нет." << endl;
-}
-
 void UniqueRand(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
-		for (int j = i; i < j; j++)
+		for (int j = 0; j < i; j++)
 		{
 			if (arr[j] == arr[i])
 			{
@@ -182,7 +132,55 @@ void UniqueRand(int arr[], int n)
 	}
 	cout << endl;
 }
+void UniqueRandTwo(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+			for (int Unique_1 = 0; Unique_1 <= i; Unique_1++)
+			{ 
+				for (int Unique_2 = 0; (Unique_2 < COLS && Unique_1 <i) || (i == Unique_1 && Unique_2<j); Unique_2++)
+				{
+					if (arr[i][j] == arr[Unique_1][Unique_2])
+					{
+						if (j == 0)
+						{
+							i--;
+							j = COLS - 1;
+						}
+						else
+							j--;
+						break;
+					}
+					
+				}
+				
+			}
+		}
+	}
+	cout << endl;
+}
 
+
+void shift(int arr[], int n)
+{
+	cout << "Выберете смещение: <-     ->" << endl;
+
+	int code = _getch();
+	//cout << code << endl;
+	if (code == 0 || code == 224)
+		code = _getch();
+	//cout << code;
+	switch (code)
+	{
+	case 75: shiftLeft(arr, n);
+		break;
+	case 77: shiftRight(arr, n);
+		break;
+	}
+}
 int shiftLeft(int arr[], const int n)
 {
 	int m;
@@ -219,28 +217,101 @@ int shiftRight(int arr[], const int n)
 	return arr[n];
 }
 
-void shift(int arr[], int n)
-{
-	cout << "Выберете смещение: <-     ->" << endl;
 
-	int code = _getch();
-	//cout << code << endl;
-	if (code == 0 || code == 224)
-		code = _getch();
-	//cout << code;
-	switch (code)
+void Search(int arr[], int n)
+{
+	int replay = 0;
+	int not_replay = 0;
+	for (int i = 0; i != n; i++)
 	{
-	case 75: shiftLeft(arr, n);
-		break;
-	case 77: shiftRight(arr, n);
-		break;
+		replay = 0;
+		for (int j = i + 1; j != n; j++)// сравнение всех элементов с текущим
+		{
+			if (arr[i] == arr[j])
+				replay++;
+		}
+		for (int l = i - 1; l >= 0; l--)// не учитывать повторения эл, которые уже были
+		{
+			if (arr[i] == arr[l])
+				replay = 0;
+		}
+		if (replay > 0)
+		{
+			cout << "Элемент " << arr[i] << " повторяется " << replay << " раз(а). " << endl;
+			not_replay++;
+		}
+	}
+	if (replay == 0 && not_replay == 0) cout << "Повторяющихся элементов нет." << endl;
+}
+void SearchTwo(int arr[ROWS][COLS], const int ROWS, const int COLS) 
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		
+		for (int j = 0; j < COLS; j++)
+		{
+			int replay = 0;
+			for (int ii = 0; ii < ROWS; ii++)
+			{
+				int jj = 0;
+				if (j == COLS - 1)
+				{
+					jj = 0;
+					ii++;
+				}
+				else
+					jj = j;
+				for (jj; jj < COLS; jj++)
+				{
+					if (arr[i][j] == arr[ii][jj])
+						replay++;
+				}
+				
+				
+			}
+			
+			if (replay > 0)
+			{
+				cout << "Элемент " << arr[i][j] << " повторяется " << replay << " раз(а). " << endl;
+			}
+		}
+
 	}
 }
 
 
+int Sum(const int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
 
-
-
+int minValueIn(const int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int maxValueIn(const int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
 
 
 
